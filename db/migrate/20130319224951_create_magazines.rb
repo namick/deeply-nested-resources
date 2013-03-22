833dcc683ae8923081fd96f2257b76d71df060b5
Nathan Amick <thoughtobject@gmail.com>
2013-03-21 18:15:42 -0700

    Add migrations

---
+++ db/migrate/20130319224951_create_magazines.rb
@@ -0,0 +1,10 @@
+class CreateMagazines < ActiveRecord::Migration
+  def change
+    create_table :magazines do |t|
+      t.string :name
+      t.integer :publisher_id
+
+      t.timestamps
+    end
+  end
+end
