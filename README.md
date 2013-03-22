f80ca83d561e3f8aab1c84de9fccb1a959d51853
Nathan Amick <thoughtobject@gmail.com>
2013-03-21 17:15:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nested3
+
+This is an example Rails app used to show [obfuscate_id](https://github.com/namick/obfuscate_id)'s use with nested resources.
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nested3::Application.config.secret_token = '5870f5da0616f04923c2adcc837075b284bf61e703b9861a7836ab1ae06324557a65ba6c138cbe5e5add1ae65767ed636ca6e6ba9658fca19a82c770a3bfa73a'
